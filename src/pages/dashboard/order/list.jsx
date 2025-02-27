27e018086cdf0a070796b1e1061337ed7ceb4122
Zohaib Khan <zohaibkhan@Zohaibs-MacBook-Pro.local>
2025-02-27 13:23:07 +0530

    Fixed Errors

--- src/pages/dashboard/order/list.jsx
+++ src/pages/dashboard/order/list.jsx
@@ -6,7 +6,7 @@ import { OrderListView } from 'src/sections/order/view';
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Company list | Dashboard - ${CONFIG.site.name}` };
+const metadata = { title: `Order list | Dashboard - ${CONFIG.site.name}` };
 
 export default function Page() {
   return (
